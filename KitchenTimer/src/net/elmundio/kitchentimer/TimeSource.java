7a37b893d600eeff69b8440e845efa78abcb1a59
Edmund Dipple <elmundio1987@gmail.com>
2013-02-02 16:47:32 +0000

    removed unneeded import

--- KitchenTimer/src/net/elmundio/kitchentimer/TimeSource.java
+++ KitchenTimer/src/net/elmundio/kitchentimer/TimeSource.java
@@ -1,8 +1,6 @@
 package net.elmundio.kitchentimer;
 
 import java.util.Observable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class TimeSource extends Observable implements Runnable{
 	
